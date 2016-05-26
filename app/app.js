5cd95c541899a90c39a91f23f67fbd2209b7587c
Mark Kelvin Pineda <mpineda@exist.com>
2016-05-26 14:08:16 +0800

    Initial commit for simple hris using angular and rest api

--- app/app.js
+++ app/app.js
@@ -8,7 +8,5 @@ angular.module('myApp', [
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
